1c809bc09ef313b846c79737cc3ad37481a5ad6a
Miguel Lemes <miguellemes005@gmail.com>
2025-04-14 13:54:57 -0300

    feat: update config middleware

--- src/http/routes/auth/authenticate-with-password.ts
+++ src/http/routes/auth/authenticate-with-password.ts
@@ -60,6 +60,7 @@ export async function authenticateWithPassword(app: FastifyInstance) {
         sameSite:"none",
         secure: true,
         maxAge: 60 * 60 * 24 * 7, // 7 dias
+        domain: undefined
       });
 
       return reply.status(201).send({ token });
