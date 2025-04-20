f71d78caa75c5eb0d047e65e8b0cfccd4e74fb6f
Miguel Lemes <miguellemes005@gmail.com>
2025-04-20 18:44:57 -0300

    feat: remove validate cookie auth middleware

--- src/http/middleware/auth.ts
+++ src/http/middleware/auth.ts
@@ -4,28 +4,19 @@ import { prisma } from "@/lib/prisma";
 import { UnauthorizedError } from "../routes/_errors/unauthorized-error";
 
 export const auth = fastifyPlugin(async (app: FastifyInstance) => {
-  app.addHook("preHandler", async (request) => {
+	app.addHook("preHandler", async (request) => {
     request.getCurrentUserId = async () => {
       try {
-        // 1. Obter o token do cookie
-        const token = request.cookies.token;
-        
-        if (!token) {
-          throw new UnauthorizedError("No auth token provided");
-        }
-
-        // 2. Verificar o JWT
-        const { sub } = await app.jwt.verify<{ sub: string }>(token);
+        const { sub } = await request.jwtVerify<{ sub: string }>();
 
         return sub;
-      } catch (error) {
+      } catch {
         throw new UnauthorizedError("Invalid auth token");
       }
     };
 
     request.getUserMembership = async (slug: string) => {
       const userId = await request.getCurrentUserId();
-      
       const member = await prisma.member.findFirst({
         where: {
           userId,
@@ -40,7 +31,7 @@ export const auth = fastifyPlugin(async (app: FastifyInstance) => {
 
       if (!member) {
         throw new UnauthorizedError(
-          `You're not a member of this workspace.`
+          `You're not a member of this workspace.`,
         );
       }
 
